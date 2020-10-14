c5fec2d07cbd7fb8b59b78c146cfe52ba0347f02
Gavin T <gt00unit@gmail.com>
2020-10-14 02:03:27 -0400

    test

--- src/getProfile.js
+++ src/getProfile.js
@@ -13,6 +13,6 @@ app.get('*', (request, response) => {
   response.status(404).send('Route not found');
 });
 
-app.listen(config.port, () => {
+/*app.listen(config.port, () => {
   console.log(`Node-server start to listen on port ${config.port}..`);
-});
+});*/
