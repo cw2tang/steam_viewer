ce48ab454c84a3f884aed91efd3bfb9bab08403c
Gavin T <gt00unit@gmail.com>
2020-10-14 18:39:59 -0400

    test API

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 //const config = require('./config');
 const { user } = require('./helper');
-const { game } = require('./helper');
 
 const app = express();
 
@@ -10,7 +9,9 @@ app.use(cors());
 
 app.get('/user/:id', user);
 
-app.get('/game/:id', game);
+app.get("/game/:id", function(req, res) {
+  console.log('https://store.steampowered.com/api/appdetails?appids=')
+});
 
 app.get('*', (request, response) => {
   response.status(404).send('Route not found');
